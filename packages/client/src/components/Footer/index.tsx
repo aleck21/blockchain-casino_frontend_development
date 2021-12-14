17008ef457d96bf506e2f15c63a54179e9caf834
IntegralTeam <repo@sfxdx.ru>
2021-12-14 17:43:54 +0300

    fix error

--- packages/client/src/components/Footer/index.tsx
+++ packages/client/src/components/Footer/index.tsx
@@ -1,22 +1,21 @@
 import React, { FC } from 'react';
 import cx from 'classnames';
-import { useTranslation } from '@project/libs/utils/i18n';
+// import { useTranslation } from '@project/libs/utils/i18n';
 import { BunnyGameLogoBig } from '@project/libs/assets/images';
 import { Image } from '@project/libs/components';
 import styles from './styles.module.scss';
 
-export const Footer:FC = () => {
-  const { t } = useTranslation('main');
+export const Footer: FC = () =>
+// const { t } = useTranslation('main');
 
-  return(
-    <div className={cx(styles.container)} >
+  (
+    <div className={cx(styles.container)}>
       <Image
         url={BunnyGameLogoBig}
         style={{
           width: 77,
-          height: 51
-        }}  
+          height: 51,
+        }}
       />
     </div>
-  )
-};
+  );
--- packages/client/src/components/Main/index.tsx
+++ packages/client/src/components/Main/index.tsx
@@ -2,11 +2,8 @@ import React, { FC } from 'react';
 import cx from 'classnames';
 import styles from './styles.module.scss';
 
-export const Main: FC = ({ children }) => {
-  return(
-    <div className={cx(styles.container)}>
-      
-    </div>
-  )
-}
-
+export const Main: FC = ({ children }) => (
+  <div className={cx(styles.container)}>
+    {children}
+  </div>
+);
