f9366dcf19666c82a365bd7d784d3715400fb9d9
IntegralTeam <repo@sfxdx.ru>
2022-01-11 17:36:41 +0700

    Add action types

--- packages/client/src/store/auth/actionTypes.ts
+++ packages/client/src/store/auth/actionTypes.ts
@@ -4,4 +4,8 @@ export enum AuthActionType {
   SetState = 'AUTH.SET_STATE',
   Logout = 'AUTH.LOGOUT',
   Refresh = 'AUTH.REFRESH',
+
+  LoginEmail = 'AUTH.LOGIN_EMAIL',
+  LoginMetamask = 'AUTH.LOGIN_METAMASK',
+  LoginGoogle = 'AUTH.LOGIN_GOOGLE',
 }
