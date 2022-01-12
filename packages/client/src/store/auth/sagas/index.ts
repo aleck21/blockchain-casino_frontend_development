4aa711b93475010558aba1848dd20dc2b760bd47
IntegralTeam <repo@sfxdx.ru>
2022-01-12 16:16:47 +0700

    Connect auth login saga

--- packages/client/src/store/auth/sagas/index.ts
+++ packages/client/src/store/auth/sagas/index.ts
@@ -1,6 +1,8 @@
 import { fork } from 'redux-saga/effects';
 import { authLogoutSaga } from './logout';
+import { authLoginSaga } from './login';
 
 export const authEffects = [
   fork(authLogoutSaga),
+  fork(authLoginSaga),
 ];
