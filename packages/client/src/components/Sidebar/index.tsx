d6d2385a58b0b023f4e4198aa788d5de2c9fefbb
IntegralTeam <repo@sfxdx.ru>
2022-01-13 17:37:45 +0700

    Rename styles file

--- packages/client/src/components/Sidebar/index.tsx
+++ packages/client/src/components/Sidebar/index.tsx
@@ -3,30 +3,30 @@ import { Image } from '@project/libs/components';
 import { BunnyGameLogoBig, BunnyGameLogoSmall } from '@project/libs/assets/images';
 import { Main } from './Main';
 import { Footer } from './Footer';
-import './styles.scss';
+import styles from './styles.module.scss';
 // import { useUser } from 'hooks';
 
 export const Sidebar: FC = () =>
 // const { isUser } = useUser();
 
   (
-    <aside className="sidebar__container">
-      <div className="sidebar__logo">
+    <aside className={styles.sidebar__container}>
+      <div className={styles.sidebar__logo}>
         <Image
           url={BunnyGameLogoBig}
-          className="sidebar__logo__image"
+          className={styles.sidebar__logo__image}
         />
       </div>
-      <div className="sidebar__logo__mobile">
+      <div className={styles.sidebar__logo__mobile}>
         <Image
           url={BunnyGameLogoSmall}
-          className="sidebar__logo__image"
+          className={styles.sidebar__logo__image}
         />
       </div>
-      <nav className="sidebar__main">
+      <nav className={styles.sidebar__main}>
         <Main />
       </nav>
-      <footer className="sidebar__footer">
+      <footer className={styles.sidebar__footer}>
         <Footer />
       </footer>
     </aside>
