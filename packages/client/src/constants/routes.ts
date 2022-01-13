e2520b0af8e5c1776bddc8bf6010212b80b60807
IntegralTeam <repo@sfxdx.ru>
2022-01-13 17:54:36 +0700

    Add routes

--- packages/client/src/constants/routes.ts
+++ packages/client/src/constants/routes.ts
@@ -2,10 +2,16 @@ export enum RouteLink {
   home = '/',
   wallet = '/wallet',
   profile = '/profile',
+  notification = '/notification',
+  roulette = '/roulette',
+  verification = '/verification',
 }
 
 export enum RouteName {
   home = 'home',
   wallet = 'wallet',
   profile = 'profile',
+  notification = 'notification',
+  roulette = 'roulette',
+  verification = 'verification',
 }
