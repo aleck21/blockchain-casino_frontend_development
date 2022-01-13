03b4bc4e2c9d2580a5dd03b1f4f6fb4714463744
IntegralTeam <repo@sfxdx.ru>
2022-01-13 17:54:23 +0700

    Replace sections with NavLink

--- packages/client/src/components/Sidebar/Main/index.tsx
+++ packages/client/src/components/Sidebar/Main/index.tsx
@@ -1,4 +1,5 @@
 import React, { FC, useContext } from 'react';
+import { NavLink } from 'react-router-dom';
 import cx from 'classnames';
 import { Image, Text } from '@project/libs/components';
 import {
@@ -11,75 +12,27 @@ import {
   IconWallet,
 } from '@project/libs/assets/images';
 import { useTranslation } from '@project/libs/utils/i18n';
-import { NavigationContext } from 'context/navigation';
 import { WidgetContext } from 'context/widget';
 import { ModalContext } from 'context/modalOpen';
+import { RouteLink } from '@project/client/src/constants';
 import styles from './styles.module.scss';
 
 export const Main: FC = () => {
   const { t } = useTranslation('main');
   const isMobile = document.documentElement.clientWidth < 460;
 
-  const {
-    menuItem,
-    onHome,
-    onWallet,
-    onProfile,
-    // onNotification,
-    // onRoulette,
-    onVerification,
-  } = useContext(NavigationContext);
-
   const { setContentWidget, closeWidget } = useContext(WidgetContext);
   const { openModal, setContentModal } = useContext(ModalContext);
-
-  const onClickHome = () => {
-    onHome();
-    // isMobile && setContentWidget('home')
-  };
-
-  const onClickWallet = () => {
-    onWallet();
-    if (isMobile) setContentWidget('wallet');
-  };
-
-  const onClickProfile = () => {
-    onProfile();
-    if (isMobile) setContentWidget('profile');
-  };
-
-  const onClickNotification = () => {
-    setContentModal('notifications');
-    openModal();
-    if (isMobile) closeWidget();
-    // onNotification();
-    // isMobile && setContentWidget('notification')
-  };
-
-  const onClickRoulette = () => {
-    setContentModal('roulette');
-    openModal();
-    if (isMobile) closeWidget();
-    // onRoulette();
-    // isMobile && setContentWidget('roulette')
-  };
-
-  const onClickVerification = () => {
-    onVerification();
-    if (isMobile) closeWidget();
-    // isMobile && setContentWidget('verification')
-  };
 
   return (
     <div className={cx(styles.container)}>
       <nav className={cx(styles.nav_top__container)}>
-        <section
-          className={cx(styles.nav__item,
-            menuItem.home ? styles.active : styles.inactive)}
-          onClick={onClickHome}
-          onKeyPress={() => {}}
-          role="menuitem"
-          tabIndex={0}
+
+        <NavLink
+          to={RouteLink.home}
+          exact
+          className={cx(styles.nav__item, styles.inactive)}
+          activeClassName={styles.active}
         >
           <Image url={IconHome} />
           <Text type="p">{t('Home')}</Text>
@@ -87,14 +40,13 @@ export const Main: FC = () => {
             url={ArrowWhite}
             className={cx(styles.arrow)}
           />
-        </section>
-        <section
-          className={cx(styles.nav__item,
-            menuItem.wallet ? styles.active : styles.inactive)}
-          onClick={onClickWallet}
-          onKeyPress={() => {}}
-          role="menuitem"
-          tabIndex={0}
+        </NavLink>
+
+        <NavLink
+          to={RouteLink.wallet}
+          exact
+          className={cx(styles.nav__item, styles.inactive)}
+          activeClassName={styles.active}
         >
           <Image url={IconWallet} />
           <Text type="p">{t('Wallet')}</Text>
@@ -102,14 +54,13 @@ export const Main: FC = () => {
             url={ArrowWhite}
             className={cx(styles.arrow)}
           />
-        </section>
-        <section
-          className={cx(styles.nav__item,
-            menuItem.profile ? styles.active : styles.inactive)}
-          onClick={onClickProfile}
-          onKeyPress={() => {}}
-          role="menuitem"
-          tabIndex={0}
+        </NavLink>
+
+        <NavLink
+          to={RouteLink.profile}
+          exact
+          className={cx(styles.nav__item, styles.inactive)}
+          activeClassName={styles.active}
         >
           <Image url={IconProfile} />
           <Text type="p">{t('Profile')}</Text>
@@ -117,14 +68,13 @@ export const Main: FC = () => {
             url={ArrowWhite}
             className={cx(styles.arrow)}
           />
-        </section>
-        <section
-          className={cx(styles.nav__item,
-            menuItem.notification ? styles.active : styles.inactive)}
-          onClick={onClickNotification}
-          onKeyPress={() => {}}
-          role="menuitem"
-          tabIndex={0}
+        </NavLink>
+
+        <NavLink
+          to={RouteLink.notification}
+          exact
+          className={cx(styles.nav__item, styles.inactive)}
+          activeClassName={styles.active}
         >
           <Image url={IconNotification} />
           <Text type="p">{t('Notification')}</Text>
@@ -132,17 +82,16 @@ export const Main: FC = () => {
             url={ArrowWhite}
             className={cx(styles.arrow)}
           />
-        </section>
+        </NavLink>
+
       </nav>
       <div className={cx(styles.separator)} />
       <nav className={cx(styles.nav_bottom__container)}>
-        <section
-          className={cx(styles.nav__item,
-            menuItem.roulette ? styles.active : styles.inactive)}
-          onClick={onClickRoulette}
-          onKeyPress={() => {}}
-          role="menuitem"
-          tabIndex={0}
+        <NavLink
+          to={RouteLink.roulette}
+          exact
+          className={cx(styles.nav__item, styles.inactive)}
+          activeClassName={styles.active}
         >
           <Image url={IconRoulette} />
           <Text type="p">{t('Roulette')}</Text>
@@ -150,14 +99,12 @@ export const Main: FC = () => {
             url={ArrowWhite}
             className={cx(styles.arrow)}
           />
-        </section>
-        <section
-          className={cx(styles.nav__item,
-            menuItem.verification ? styles.active : styles.inactive)}
-          onClick={onClickVerification}
-          onKeyPress={() => {}}
-          role="menuitem"
-          tabIndex={0}
+        </NavLink>
+        <NavLink
+          to={RouteLink.verification}
+          exact
+          className={cx(styles.nav__item, styles.inactive)}
+          activeClassName={styles.active}
         >
           <Image url={IconVerification} />
           <Text type="p">{t('Verification')}</Text>
@@ -165,7 +112,7 @@ export const Main: FC = () => {
             url={ArrowWhite}
             className={cx(styles.arrow)}
           />
-        </section>
+        </NavLink>
       </nav>
     </div>
   );
