b7ca4197578bac272f81426f265d56adb856ff5a
IntegralTeam <repo@sfxdx.ru>
2022-01-18 10:26:20 +0300

    Remove AboutBGD

--- packages/client/src/components/ModalWindow/index.tsx
+++ packages/client/src/components/ModalWindow/index.tsx
@@ -12,7 +12,6 @@ import { Transactions } from 'containers/profile/Profile/Modals/Transactions';
 import { Notification, Roulette } from 'containers';
 import { SignInModal, RegisterModal } from 'containers/modals';
 import { CloseIcon } from '@project/libs/assets/images';
-import { AboutBGD } from 'containers/home/Home/AboutBGD';
 import styles from './styles.module.scss';
 
 const modals: Record<string, ReactNode> = {
@@ -25,7 +24,6 @@ const modals: Record<string, ReactNode> = {
   roulette: <Roulette />,
   signInModal: <SignInModal />,
   registerModal: <RegisterModal />,
-  aboutBgd: <AboutBGD />,
 };
 
 export const ModalWindow: React.FC = () => {
