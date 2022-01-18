56172f5f4f877f13d439f253b477f605dc811fad
IntegralTeam <repo@sfxdx.ru>
2022-01-18 10:19:03 +0300

    Fix Paper on Verification & Wallet

--- packages/client/src/containers/verification/Verification/index.tsx
+++ packages/client/src/containers/verification/Verification/index.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import cx from 'classnames';
-import { Image, Text, Paper } from '@project/libs/components';
+import { Image, Text } from '@project/libs/components';
 import { BackArrow } from '@project/libs/assets/images';
 import { useTranslation } from '@project/libs/utils/i18n';
 import styles from './styles.module.scss';
@@ -23,33 +23,31 @@ export const Verification: React.FC = () => {
   };
 
   return (
-    <div className={cx(styles.verification__container)}>
-      <Paper className={cx(styles.verification__paper)}>
-        <header className={cx(styles.verification__header)}>
-          <div
-            className={cx(
-              styles.verification__backButton,
-              stage === 1 && styles.hidden,
-            )}
-            onClick={goBack}
-            role="button"
-            tabIndex={0}
-            onKeyPress={() => {}}
-          >
-            <Image url={BackArrow} />
-            <span className={cx(styles.backButton__text)}>Go back</span>
-          </div>
-          <Text type="h2">
-            {t('Verification')}
-          </Text>
-        </header>
-        <section className={cx(styles.verification__main)}>
-          {stage === 1 && <Stage1 goNextStage={goNextStage} />}
-          {stage === 2 && <Stage2 goNextStage={goNextStage} />}
-          {stage === 3 && <Stage3 goNextStage={goNextStage} />}
-          {stage === 4 && <Stage4 />}
-        </section>
-      </Paper>
+    <div className={cx(styles.verification__paper)}>
+      <header className={cx(styles.verification__header)}>
+        <div
+          className={cx(
+            styles.verification__backButton,
+            stage === 1 && styles.hidden,
+          )}
+          onClick={goBack}
+          role="button"
+          tabIndex={0}
+          onKeyPress={() => {}}
+        >
+          <Image url={BackArrow} />
+          <span className={cx(styles.backButton__text)}>Go back</span>
+        </div>
+        <Text type="h2">
+          {t('Verification')}
+        </Text>
+      </header>
+      <section className={cx(styles.verification__main)}>
+        {stage === 1 && <Stage1 goNextStage={goNextStage} />}
+        {stage === 2 && <Stage2 goNextStage={goNextStage} />}
+        {stage === 3 && <Stage3 goNextStage={goNextStage} />}
+        {stage === 4 && <Stage4 />}
+      </section>
     </div>
   );
 };
--- packages/client/src/containers/wallet/Wallet/index.tsx
+++ packages/client/src/containers/wallet/Wallet/index.tsx
@@ -3,7 +3,6 @@ import cx from 'classnames';
 import { useTranslation } from '@project/libs/utils/i18n';
 import { HeadPage } from 'components/HeadPage';
 import { WalletTabsProvider } from 'context/walletTabs';
-import { Paper } from '@project/libs/components/common';
 import styles from './styles.module.scss';
 import { TableGrid } from './components/TableGrid';
 import { BalanceItem } from './components/BalaceItem';
@@ -23,9 +22,9 @@ const Wallet: FC = () => {
           <HeadPage>
             {t('Wallet')}
           </HeadPage>
-          <Paper className={cx(styles.wallet__panel__paper)}>
+          <section className={cx(styles.wallet__panel__paper)}>
             <Panel />
-          </Paper>
+          </section>
         </div>
         <div className={cx(styles.wallet__balance)}>
           <TableGrid title={t('Balance')}>
@@ -37,11 +36,9 @@ const Wallet: FC = () => {
             ))}
           </TableGrid>
         </div>
-        <div className={cx(styles.wallet__bgd_dashboard)}>
-          <Paper className={cx(styles.bgd_dashboard__container)}>
-            <BGDDashboard />
-          </Paper>
-        </div>
+        <section className={cx(styles.bgd_dashboard__container)}>
+          <BGDDashboard />
+        </section>
         <TabsMobile />
       </div>
     </WalletTabsProvider>
