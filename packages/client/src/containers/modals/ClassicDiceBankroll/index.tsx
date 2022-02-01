ab69becbc3844d86fc79e035252a7a45f7e987c4
IntegralTeam <repo@sfxdx.ru>
2022-02-01 10:06:00 +0300

    Top10 from WithdrawTab

--- packages/client/src/containers/modals/ClassicDiceBankroll/index.tsx
+++ packages/client/src/containers/modals/ClassicDiceBankroll/index.tsx
@@ -1,9 +1,10 @@
 import React, { memo } from 'react';
-import { InfoIconBlue } from '@project/libs/assets/images';
+import { AvatarDefault, InfoIconBlue } from '@project/libs/assets/images';
 import { Button, Image, Text } from '@project/libs/components';
 import { useTranslation } from '@project/libs/utils/i18n';
 import { Tabs } from './Tabs';
 import styles from './styles.module.scss';
+import { withdraw } from './contentDemo';
 
 const ClassicDiceBankroll = memo(() => {
   const { t } = useTranslation('main');
@@ -18,6 +19,36 @@ const ClassicDiceBankroll = memo(() => {
         <Text type="h3">
           {t('Top 10')}
         </Text>
+        <table>
+          <thead>
+            <tr>
+              <th>
+                {t('Player')}
+              </th>
+              <th>
+                {t('Amount')}
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {withdraw.top10.map((row) => (
+              <tr key={row.id}>
+                <td>
+                  <Image url={AvatarDefault} />
+                  &emsp;
+                  {row.userName}
+                </td>
+                <td className={styles.right}>
+                  {row.amount.quantity}
+                  {' '}
+                  <span>
+                    {row.amount.currency}
+                  </span>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
       <article className={styles.bankroll__footer}>
         <Button
